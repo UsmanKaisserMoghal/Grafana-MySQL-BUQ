044c215b8df69c3228bdd5819af5e95abdb3599d
Usman Kaisser Moghal <usmank.moghal@gmail.com>
2024-01-14 21:32:00 +0500

    Coaches Dashboard V1

---
+++ Coaches Dashboard/Payment per Coach.sql
@@ -0,0 +1,41 @@
+SELECT
+    loc.name as Location,
+    staff.name as Coach,
+    COUNT(DISTINCT meetings.id) as Classes,
+    COUNT(DISTINCT rez.users_id) as Attendance,
+    COUNT(DISTINCT rez.id) as Reservations,
+    CASE
+        WHEN "$cfrate" < 0 OR "$ccom" < 0 THEN 'ERR'
+        WHEN "$cfrate" IS NULL OR "$ccom" IS NULL THEN 0
+        WHEN "$cfrate" > 0 AND "$ccom" = 0 THEN "$cfrate" * COUNT(DISTINCT meetings.id)
+        WHEN "$ccom" IS NOT NULL THEN
+            CASE
+                WHEN "$ccomthresh" = 0 THEN "$ccom" * COUNT(DISTINCT rez.id)
+                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) = "$ccomthresh") THEN 0
+                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) > "$ccomthresh") THEN (COUNT(DISTINCT rez.id) - "$ccomthresh") * "$ccom"
+                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) < "$ccomthresh") THEN 0
+                ELSE
+                    'ERR'
+            END
+        ELSE NULL
+    END as Payment
+FROM
+    dev.meetings as meetings
+JOIN
+    dev.reservations as rez ON meetings.id = rez.meetings_id
+JOIN
+    dev.staff as staff ON meetings.staff_id = staff.id
+JOIN
+    dev.locations as loc ON meetings.locations_id = loc.id
+WHERE
+    rez.created_at BETWEEN CONVERT_TZ(FROM_UNIXTIME($__unixEpochFrom()), '+00:00', '-05:00')
+                      AND CONVERT_TZ(FROM_UNIXTIME($__unixEpochTo()), '+00:00', '-05:00')
+    AND staff.job = "Coach"
+    AND (
+        "$ccheck" = "Yes"
+        OR ("$ccheck" = "No" AND rez.cancelled = 0)
+    )
+GROUP BY
+    meetings.staff_id,
+    loc.id
+LIMIT 10;
