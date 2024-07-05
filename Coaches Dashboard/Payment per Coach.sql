e80d93f7076a65c9c42f79c67a4b35e52c0281fa
Usman Kaisser Moghal <usmank.moghal@gmail.com>
2024-07-05 14:49:00 +0500

    Coaches Dashboard V3

--- Coaches Dashboard/Payment per Coach.sql
+++ Coaches Dashboard/Payment per Coach.sql
@@ -5,20 +5,38 @@ SELECT
     COUNT(DISTINCT rez.users_id) as Attendance,
     COUNT(DISTINCT rez.id) as Reservations,
     CASE
-        WHEN "$cfrate" < 0 OR "$ccom" < 0 THEN 'ERR'
-        WHEN "$cfrate" IS NULL OR "$ccom" IS NULL THEN 0
-        WHEN "$cfrate" > 0 AND "$ccom" = 0 THEN "$cfrate" * COUNT(DISTINCT meetings.id)
-        WHEN "$cfrate" REGEXP '^[0-9]+$' = 0 OR "$ccom" REGEXP '^[0-9]+$' = 0 OR "$ccomthresh" REGEXP '^[0-9]+$' = 0 THEN 'ERR'
-        WHEN "$ccom" > 0 THEN
+        WHEN staff.flatRate IS NULL OR staff.commissionPrice IS NULL OR staff.commissionThreshold IS NULL THEN
             CASE
-                WHEN "$ccomthresh" = 0 THEN ("$ccom" * COUNT(DISTINCT rez.id)) + ("$cfrate" * COUNT(DISTINCT meetings.id))
-                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) = "$ccomthresh") THEN "$cfrate" * COUNT(DISTINCT meetings.id)
-                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) > "$ccomthresh") THEN ((COUNT(DISTINCT rez.id) - "$ccomthresh") * "$ccom") + ("$cfrate" * COUNT(DISTINCT meetings.id))
-                WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) < "$ccomthresh") THEN "$cfrate" * COUNT(DISTINCT meetings.id)
-                ELSE
-                    'ERR'
+                WHEN "$cfrate" < 0 OR "$ccom" < 0 THEN 'ERR'
+                WHEN "$cfrate" IS NULL OR "$ccom" IS NULL THEN 0
+                WHEN "$cfrate" > 0 AND "$ccom" = 0 THEN "$cfrate" * COUNT(DISTINCT meetings.id)
+                WHEN "$cfrate" REGEXP '^[0-9]+$' = 0 OR "$ccom" REGEXP '^[0-9]+$' = 0 OR "$ccomthresh" REGEXP '^[0-9]+$' = 0 THEN 'ERR'
+                WHEN "$ccom" > 0 THEN
+                    CASE
+                        WHEN "$ccomthresh" = 0 THEN ("$ccom" * COUNT(DISTINCT rez.id)) + ("$cfrate" * COUNT(DISTINCT meetings.id))
+                        WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) = "$ccomthresh") THEN "$cfrate" * COUNT(DISTINCT meetings.id)
+                        WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) > "$ccomthresh") THEN ((COUNT(DISTINCT rez.id) - "$ccomthresh") * "$ccom") + ("$cfrate" * COUNT(DISTINCT meetings.id))
+                        WHEN ("$ccomthresh" > 0 AND COUNT(DISTINCT rez.id) < "$ccomthresh") THEN "$cfrate" * COUNT(DISTINCT meetings.id)
+                        ELSE 'ERR'
+                    END
+                ELSE NULL
             END
-        ELSE NULL
+        ELSE
+            CASE
+                WHEN staff.flatRate < 0 OR staff.commissionPrice < 0 THEN 'ERR'
+                WHEN staff.flatRate IS NULL OR staff.commissionPrice IS NULL THEN 0
+                WHEN staff.flatRate > 0 AND staff.commissionPrice = 0 THEN staff.flatRate * COUNT(DISTINCT meetings.id)
+                WHEN staff.flatRate REGEXP '^[0-9]+$' = 0 OR staff.commissionPrice REGEXP '^[0-9]+$' = 0 OR staff.commissionThreshold REGEXP '^[0-9]+$' = 0 THEN 'ERR'
+                WHEN staff.commissionPrice > 0 THEN
+                    CASE
+                        WHEN staff.commissionThreshold = 0 THEN (staff.commissionPrice * COUNT(DISTINCT rez.id)) + (staff.flatRate * COUNT(DISTINCT meetings.id))
+                        WHEN (staff.commissionThreshold > 0 AND COUNT(DISTINCT rez.id) = staff.commissionThreshold) THEN staff.flatRate * COUNT(DISTINCT meetings.id)
+                        WHEN (staff.commissionThreshold > 0 AND COUNT(DISTINCT rez.id) > staff.commissionThreshold) THEN ((COUNT(DISTINCT rez.id) - staff.commissionThreshold) * staff.commissionPrice) + (staff.flatRate * COUNT(DISTINCT meetings.id))
+                        WHEN (staff.commissionThreshold > 0 AND COUNT(DISTINCT rez.id) < staff.commissionThreshold) THEN staff.flatRate * COUNT(DISTINCT meetings.id)
+                        ELSE 'ERR'
+                    END
+                ELSE NULL
+            END
     END as Payment
 FROM
     dev.meetings as meetings
@@ -33,10 +51,9 @@ WHERE
                       AND CONVERT_TZ(FROM_UNIXTIME($__unixEpochTo()), '+00:00', '-05:00')
     AND meetings.brands_id = $brand_id 
     AND (
-        "$ccheck" = "Yes"
-        OR ("$ccheck" = "No" AND rez.cancelled = 0)
+        (CASE WHEN staff.include_noShows IS NULL THEN "$ccheck" ELSE staff.include_noShows END) = 1
+        OR ((CASE WHEN staff.include_noShows IS NULL THEN "$ccheck" ELSE staff.include_noShows END) = 0 AND rez.cancelled = 0)
     )
 GROUP BY
     meetings.staff_id,
     loc.id
-LIMIT 10;
